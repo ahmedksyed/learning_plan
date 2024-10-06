cdd7913af704611a2a185df90ebbe7955cba390f
Ahmed Syed <ahmedk.syed90@gmail.com>
2024-10-06 18:53:53 +0530

    Day 2, tailwind css crash course
    completed day 2 tailwind css crash course by Traversy Media

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","index.html"],
+  content: ["./src/**/*.{html,js}","*"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
     extend: {},
   },
   plugins: [],
