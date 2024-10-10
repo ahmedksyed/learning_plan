8f872b899742af3a6e7d87088fff8e88e8e8be93
Ahmed Syed <ahmedk.syed90@gmail.com>
2024-10-10 21:57:46 +0530

    Added interactivity
    Added interactivity to the previously built landing page, modified tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Enables dark mode using the 'dark' class
   content: ["./src/**/*.{html,js}","*"],
   theme: {
     container: {
       center: true,
       padding: "2rem",
     },
-    extend: {},
+    extend: {
+      fontSize: {
+        mammoth: '6rem',
+      },
+      colors: {
+        'success' : '#0f9b0f',
+        'error'   : '#ff0000',
+        'warning'  : '#ff8c00',
+      }
+    },
   },
   plugins: [],
 }
